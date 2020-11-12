8be61a61b329406bfb1f1bf0f804f6107ff9d40d
atharvajoshi27 <atharvamj270800@gmail.com>
2020-11-12 20:15:21 +0530

    Unnecessary removal

--- request_handler.py
+++ request_handler.py
@@ -17,6 +17,90 @@ import base64
 
 from conf import *
 
+
+CRLF = "\r\n"
+
+ERROR_RESPONSE = """
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+		<title>Error response</title>
+	</head>
+	<body>
+		<p><b>%(code)s</b>. %(explain)s.</p>
+		<p>ERROR_HAS_OCCURED</p>
+	</body>
+</html>
+"""
+
+STATUS = {
+	 100 : "Continue",
+	 101 : "Switching Protocol",
+	 102 : "Processing",
+	 103 : "Early Hints",
+	 200 : "OK",
+	 201 : "Created",
+	 202 : "Accepted",
+	 203 : "Non-Authoritative Information",
+	 204 : "No Content",
+	 205 : "Reset Content",
+	 206 : "Partial Content",
+	 207 : "Multi-Status",
+	 208 : "Already Reported",
+	 226 : "IM Used",
+	 300 : "Multiple Choice",
+	 301 : "Moved Permanently",
+	 302 : "Found",
+	 303 : "See Other",
+	 304 : "Not Modified",
+	 305 : "Use Proxy",
+	 306 : "Unused",
+	 307 : "Temporary Redirect",
+	 308 : "Permanent Redirect",
+	 400 : "Bad Request",
+	 401 : "Unauthorized",
+	 402 : "Payment Required",
+	 403 : "Forbidden",
+	 404 : "Not Found",
+	 405 : "Method Not Allowed",
+	 406 : "Not Acceptable",
+	 407 : "Proxy Authentication Required",
+	 408 : "Request Timeout",
+	 409 : "Conflict",
+	 410 : "Gone",
+	 411 : "Length Required",
+	 412 : "Precondition Failed",
+	 413 : "Payload Too Large",
+	 414 : "URI Too Long",
+	 415 : "Unsupported Media Type",
+	 416 : "Range Not Satisfiable",
+	 417 : "Expectation Failed",
+	 418 : "I'm a teapot",
+	 421 : "Misdirected Request",
+	 422 : "Unprocessable Emtity",
+	 423 : "Locked",
+	 424 : "Failed Dependency",
+	 425 : "Too Early",
+	 426 : "Upgrade Required",
+	 428 : "Precondition Required",
+	 429 : "Too Many Requests",
+	 431 : "Request Header Fields Too Large",
+	 451 : "Unavailable For Legal Reasons",
+	 500 : "Internal Server Error",
+	 501 : "Not Implemented",
+	 502 : "Bad Gateway",
+	 503 : "Service Unavailable",
+	 504 : "Gateway Timeout",
+	 505 : "HTTP Version Not Supported",
+	 506 : "Variant Also Negotiates",
+	 507 : "Insufficient Storage",
+	 508 : "Loop Detected",
+	 510 : "Not Extended",
+	 511 : "Network Authentication Required",
+}
+
+
 def use_logger(access=1, post=0, error=0):
 	if post == 1:
 		filename = f"{BASE_DIR}/log/post.log"
@@ -240,7 +324,7 @@ class RequestHandler():
 	def setstatuscodeandtype(self, status_code):
 		self.status_code = status_code
 		self.status_type = STATUS[status_code]
-		
+
 	def GET_method(self):
 		if self.path in CONFIDENTIAL:
 			try :
